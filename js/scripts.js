d4ccc74a449cfa379c3c04ac674d3cc75de7bb6a
KameronJohnson <kameronjohnson@gmail.com>
2015-05-28 22:10:43 -0700

    add html search form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  $('form').submit(function(event) {
+    event.prevenDefault();
+    var $searchField = $('#search');
 
+
+  })
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var
+    $.getJSON(flickrAPI, flickrData, displayPhotos);
+
+});
 
 });
