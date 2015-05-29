c4a5e26195961c241f68a89c3c72e90ebe69bf5b
KameronJohnson <kameronjohnson@gmail.com>
2015-05-28 22:47:58 -0700

    display user search

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('form').submit(function(event) {
-    event.prevenDefault();
+    event.preventDefault();
     var $searchField = $('#search');
     //AJAX
     var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
@@ -14,7 +14,7 @@ $(document).ready(function(){
       $.each(data.items, function(i, photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photos.media.m + '"></a></li>';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       });
       photoHTML += '</ul>';
       $('#photos').html(photoHTML);
